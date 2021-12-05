c97197e3d78c937569dfb1ead122a7f950f77e6f
Charles Carpenter <cscarp0@g.uky.edu>
2021-12-05 09:49:03 -0500

    Added full path to ansible to check if installed

--- experiment_bootstrap/install_ansible.sh
+++ experiment_bootstrap/install_ansible.sh
@@ -6,7 +6,8 @@ sudo apt install -y python3-pip
 pip3 --version 
 echo "Installing ansible"
 pip install ansible 
-ansible --version 
-ansible-galaxy --version 
+# Not sure why need to hard code full path here
+/home/ansible/.local/bin/ansible --version 
+/home/ansible/.local/bin/ansible-galaxy --version 
  
 echo "Finished!" 
