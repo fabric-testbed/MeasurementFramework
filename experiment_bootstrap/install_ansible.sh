733dbf31f14cb27b98ac157e8d79afb0bc723a6c
Charles Carpenter <cscarp0@g.uky.edu>
2021-12-05 09:51:02 -0500

    Adde ---- for visibility:

--- experiment_bootstrap/install_ansible.sh
+++ experiment_bootstrap/install_ansible.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Updating apt"
+echo "-----Updating apt-----"
 sudo apt update   
-echo "Installing pip3"
+echo "-----Installing pip3-----"
 sudo apt install -y python3-pip   
 pip3 --version 
-echo "Installing ansible"
+echo "-----Installing ansible-----"
 pip install ansible 
 # Not sure why need to hard code full path here
 /home/ansible/.local/bin/ansible --version 
 /home/ansible/.local/bin/ansible-galaxy --version 
  
-echo "Finished!" 
+echo "-----Finished!-----" 
