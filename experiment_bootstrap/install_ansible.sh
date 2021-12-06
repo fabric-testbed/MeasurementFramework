20aec5fbec90905b2aeb94c566bfc386a067171d
Charles Carpenter <cscarp0@g.uky.edu>
2021-12-06 12:26:03 -0500

    Added reuirements.txt to bootstrap script

--- experiment_bootstrap/install_ansible.sh
+++ experiment_bootstrap/install_ansible.sh
@@ -4,19 +4,23 @@ sudo apt update
 echo "-----Installing pip3-----"
 sudo apt install -y python3-pip   
 pip3 --version 
-echo "-----Installing ansible-----"
-pip install ansible 
+echo "-----Installing python requirements-----"
+
+pip3 install fabric_prometheus/ansible/roles/fabric_experiment/meta/requirements.txt
+
+#pip install ansible 
 # Not sure why need to hard code full path here
+/home/ansible/.local/bin/pip --version 
 /home/ansible/.local/bin/ansible --version 
 /home/ansible/.local/bin/ansible-galaxy --version 
  
-
 
 echo "-----Install Galaxy Roles-----"
 /home/ansible/.local/bin/ansible-galaxy install -r fabric_prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
 echo "-----Install Galaxy Collections----"
 /home/ansible/.local/bin/ansible-galaxy collection install -r fabric_prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
 
-
+echo "pip freeze:"
+pip freeze
 
 echo "-----Finished!-----" 
