3151a54a60cf8d1b7cf3be0168923998340c939a
Charles Carpenter <cscarp0@g.uky.edu>
2022-02-14 15:16:56 -0500

    Changed missed fabric_prometheus to prometheus.

--- instrumentize/experiment_bootstrap/install_ansible.sh
+++ instrumentize/experiment_bootstrap/install_ansible.sh
@@ -6,7 +6,7 @@ sudo apt install -y python3-pip
 pip3 --version 
 echo "-----Installing python requirements-----"
 
-pip3 install -r fabric_prometheus/ansible/roles/fabric_experiment/meta/requirements.txt
+pip3 install -r prometheus/ansible/roles/fabric_experiment/meta/requirements.txt
 
 #pip install ansible 
 # Not sure why need to hard code full path here
@@ -16,9 +16,9 @@ pip3 install -r fabric_prometheus/ansible/roles/fabric_experiment/meta/requireme
  
 
 echo "-----Install Galaxy Roles-----"
-/home/ansible/.local/bin/ansible-galaxy install -r fabric_prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
+/home/ansible/.local/bin/ansible-galaxy install -r prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
 echo "-----Install Galaxy Collections----"
-/home/ansible/.local/bin/ansible-galaxy collection install -r fabric_prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
+/home/ansible/.local/bin/ansible-galaxy collection install -r prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
 
 echo "pip freeze:"
 pip freeze
