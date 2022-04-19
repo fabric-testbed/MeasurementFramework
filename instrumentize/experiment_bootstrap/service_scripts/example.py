2be3e9a5497a2f522e1ad301f781e1437d9124c1
Charles Carpenter <cscarp0@g.uky.edu>
2022-04-19 10:38:06 -0400

    Adding services bootstrap for experiment.

---
+++ instrumentize/experiment_bootstrap/service_scripts/example.py
@@ -0,0 +1,24 @@
+# Example of a minimal setup for MeasurementFramework service setup for Fabric Experiment on meas node.
+# Idea is to copy all needed files to the services directory setup on the meas node.
+
+import argparse
+import os
+
+# This script will be called by the bootstrap process while setting up the meas node.
+# The call will include the path for the directory setup for the service. 
+parser = argparse.ArgumentParser(description='Set up service to directory.')
+parser.add_argument('--service_dir', help="Destination directory to copy service files into.")
+
+def copy_files(src_dir, dst_dir):
+    os.system(f"cp -r {src_dir}/* {dst_dir}")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    
+    # src_dir is the path to the folder containing the files for controlling the service from mflib.
+    # In this simple case all the files are in one folder. 
+    src_dir = "../../../user_services/example/service_commands"
+    print(f"Copying files and subfolders from {src_dir} to {args.service_dir}")
+    copy_files(src_dir, args.service_dir)
+    # Note the files do not need to come from a simple local copy as shown here. They could be pulled from anywhere such as a git repo or auto generated.
+    
