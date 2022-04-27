0d88cc4366467472783eb9dcddb8990f4eeec397
Charles Carpenter <cscarp0@g.uky.edu>
2022-04-27 07:33:57 -0400

    Added full paths

--- instrumentize/experiment_bootstrap/install_ansible.sh
+++ instrumentize/experiment_bootstrap/install_ansible.sh
@@ -6,7 +6,7 @@ sudo apt install -y python3-pip
 pip3 --version 
 echo "-----Installing python requirements-----"
 
-pip3 install -r prometheus/ansible/roles/fabric_experiment/meta/requirements.txt
+pip3 install -r /home/mfuser/mf_git/instrumentize/prometheus/ansible/roles/fabric_experiment/meta/requirements.txt
 
 #pip install ansible 
 # Not sure why need to hard code full path here
@@ -16,9 +16,9 @@ pip3 install -r prometheus/ansible/roles/fabric_experiment/meta/requirements.txt
  
 
 echo "-----Install Galaxy Roles-----"
-/home/mfuser/.local/bin/ansible-galaxy install -r prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
+/home/mfuser/.local/bin/ansible-galaxy install -r /home/mfuser/mf_git/instrumentize/prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
 echo "-----Install Galaxy Collections----"
-/home/mfuser/.local/bin/ansible-galaxy collection install -r prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
+/home/mfuser/.local/bin/ansible-galaxy collection install -r /home/mfuser/mf_git/instrumentize/prometheus/ansible/roles/fabric_experiment/meta/requirements.yml
 
 echo "pip freeze:"
 pip freeze
