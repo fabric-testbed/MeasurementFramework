84ea43ae7a165be4c5c92022599626a8f053230a
Charles Carpenter <cscarp0@g.uky.edu>
2022-04-28 11:02:36 -0400

    Adding prometheus service scripts.

---
+++ user_services/prometheus/service_commands/info.py
@@ -0,0 +1,21 @@
+# Get prometheus info.
+
+import os
+import json 
+
+
+def main():
+    # Assume it will go well.
+    retVal = {
+        "success": True,
+        "msg": "Not yet implmented."
+    }
+
+    # read some info
+
+    # get the latest recap
+
+
+    
+if __name__ == "__main__":
+    main()
