8cf755fb6a9913b141a1caebcfd9cfe16467688d
Charles Carpenter <cscarp0@g.uky.edu>
2022-04-29 12:34:40 -0400

    Prometheus service additions.

--- user_services/prometheus/service_commands/info.py
+++ user_services/prometheus/service_commands/info.py
@@ -3,14 +3,18 @@
 import os
 import json 
 
+import prom_utilites as pu
 
 def main():
     # Assume it will go well.
     retVal = {
         "success": True,
-        "msg": "Not yet implmented."
+        "msg": ""
     }
 
+    retVal["grafana_admin_password"] = pu.get_grafana_admin_password()
+
+    print( pu.get_json_string())
     # read some info
 
     # get the latest recap
