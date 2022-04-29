7ba90969dc499b5b4681d82a184718a9ea500f6d
Charles Carpenter <cscarp0@g.uky.edu>
2022-04-29 15:26:44 -0400

    Updating prom service

--- user_services/prometheus/service_commands/info.py
+++ user_services/prometheus/service_commands/info.py
@@ -1,25 +1,29 @@
 # Get prometheus info.
-
+  
 import os
-import json 
+import json
 
 import prom_utilites as pu
 
 def main():
+
+    ret_val = {}
+
+    data = pu.get_data()
+
+    if data:
+        if "grafana_admin_password" in data:
+            ret_val["grafana_admin_password"] = pu.get_grafana_admin_password()
+        if "ansible_output" in data:
+            ret_val["ansible_output"]  = pu.get_last_ansible_out_str()
     # Assume it will go well.
-    retVal = {
-        "success": True,
-        "msg": ""
-    }
+    else:
+        # get everything for now
+        ret_val["grafana_admin_password"] = pu.get_grafana_admin_password()
+        ret_val["ansible_output"]  = pu.get_last_ansible_out_str()
 
-    retVal["grafana_admin_password"] = pu.get_grafana_admin_password()
-
-    print( pu.get_json_string())
-    # read some info
-
-    # get the latest recap
+    print( pu.get_json_string(ret_val) )
 
 
-    
 if __name__ == "__main__":
     main()
