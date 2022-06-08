4097d876bd6979383a7c7696709faaae48ed8f76
Song <ywsong2@uky.edu>
2022-06-08 15:28:51 -0400

    Revert "Create service dir before calling python script."
    This reverts commit f109d3e47bdd8f5add5a25b945fe30fe352d4bda.

--- instrumentize/experiment_bootstrap/bootstrap.sh
+++ instrumentize/experiment_bootstrap/bootstrap.sh
@@ -6,6 +6,5 @@
 /home/mfuser/mf_git/instrumentize/experiment_bootstrap/install_ansible.sh
 
 # Setup service directories.
-mkdirs /home/mfuser/services
 /usr/bin/python3 /home/mfuser/mf_git/instrumentize/experiment_bootstrap/setup_service_dirs.py 
 
