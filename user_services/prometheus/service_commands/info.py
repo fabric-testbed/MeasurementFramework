f66407efd6dbcd24e39bd357f63b8660a2fa2e8d
Song <ywsong2@uky.edu>
2022-06-08 15:28:51 -0400

    Revert "Prometheus service additions."
    This reverts commit 8cf755fb6a9913b141a1caebcfd9cfe16467688d.

--- user_services/prometheus/service_commands/info.py
+++ user_services/prometheus/service_commands/info.py
@@ -3,18 +3,14 @@
 import os
 import json 
 
-import prom_utilites as pu
 
 def main():
     # Assume it will go well.
     retVal = {
         "success": True,
-        "msg": ""
+        "msg": "Not yet implmented."
     }
 
-    retVal["grafana_admin_password"] = pu.get_grafana_admin_password()
-
-    print( pu.get_json_string())
     # read some info
 
     # get the latest recap
