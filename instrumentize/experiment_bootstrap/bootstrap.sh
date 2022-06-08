6a238d3851bdfb20d5e92e7f9dd60c09e0dfdddd
Song <ywsong2@uky.edu>
2022-06-08 15:28:51 -0400

    Revert "Adding to bootstrap script"
    This reverts commit a35d7e02b153ca73e3c12a7f69bf08b0c0682997.

--- instrumentize/experiment_bootstrap/bootstrap.sh
+++ instrumentize/experiment_bootstrap/bootstrap.sh
@@ -2,9 +2,5 @@
 
 # Runs all the scripts needed to setup the meas node.
 
-# Install requriremets for ansible - python3, pip3, galaxies etc..
 /home/mfuser/mf_git/instrumentize/experiment_bootstrap/install_ansible.sh
-
-# Setup service directories.
-/usr/bin/python3 /home/mfuser/mf_git/instrumentize/experiment_bootstrap/setup_service_dirs.py 
 
