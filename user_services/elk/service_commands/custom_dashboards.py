ac4b68af4ad9e696e87abf86de67fcecbf155f05
cpemberton3 <81195431+cpemberton3@users.noreply.github.com>
2022-08-17 15:12:35 -0400

    Update custom_dashboards.py

--- user_services/elk/service_commands/custom_dashboards.py
+++ user_services/elk/service_commands/custom_dashboards.py
@@ -10,7 +10,7 @@ import elk_utilities as eu
 
 
 def import_dashboards():
-    ret_val = {}
+    ret_val = ''
     logging.info("Starting Dashboard Imports")
     try:
       meas_node_ip = socket.gethostbyname(socket.gethostname())
@@ -22,15 +22,16 @@ def import_dashboards():
       #os.chdir('../dashboards')
       #for file in os.listdir(os.getcwd()):
       
-      #for file in os.listdir("/home/mfuser/mf_git/instrumentize/dashboards"):
-      for file in os.listdir(eu.dashboards_dir):
+      #for file in os.listdir("/home/mfuser/mf_git/instrumentize/dashboards"):a
+      for file in os.listdir(eu.files_dir):
         if file.endswith('.ndjson'):
           logging.info("Uploading " + file)
           api_ip = 'http://' + meas_node_ip + '/api/saved_objects/_import?createNewCopies=true'
           headers = {'kbn-xsrf': 'true',}
-          files = {'file': (file, open(file, 'rb')),}
+          files = {'file': (file, open(os.path.join(eu.files_dir, file), 'rb')),}
           response = requests.post(api_ip, headers=headers, files=files, auth=(username, password))
-          ret_val["msg"] += f"Uploaded dashboard {file}. "
+          ret_val += f"Uploaded dashboard {file}. "
     except Exception as e:
         logging.error(f"Error in importing dashboards: {e}")
-        ret_val["msg"] += f"Error in importing dashboards: {e} "
+        ret_val += f"Error in importing dashboards: {e} "
+    return ret_val
