20be70c882a84e223a20845e9f493646b6fb9b54
mamihayashida <mami.hayashida@uky.edu>
2023-04-03 12:54:29 -0400

    new feature: option to use Python time module

--- user_services/owl/owl/sock_ops/udp_sender.py
+++ user_services/owl/owl/sock_ops/udp_sender.py
@@ -15,9 +15,17 @@ from time_ops import ptp
 from threading import Timer
 
 class UDP_sender(object):
-    def __init__(self, ptp_so_file, interval, dst_ip, dst_port, start_num):
-        self.ptp_device = self.find_ptp_device_name()
-        self.ptp_so_file = ptp_so_file
+    def __init__(self, interval, dst_ip, dst_port, start_num, 
+                 ptp_so_file=None, sys_clock=True):
+        
+        if sys_clock:
+            self.ptp_device = None
+            print("Using system clock")
+        else:
+            self.ptp_device = self.find_ptp_device_name()
+            self.ptp_so_file = ptp_so_file
+            print("Using PTP device")
+
         self._timer     = None
         self.interval   = interval
         self.is_running = False
@@ -28,6 +36,7 @@ class UDP_sender(object):
         self.seq_n = start_num
         
         self.start()
+
 
     def find_ptp_device_name(self):
         result=os.popen("sudo ps -ef |grep phc2sys").read()
@@ -43,24 +52,32 @@ class UDP_sender(object):
         else:
             sys.exit("Cannot find running ptp device")
 
+
     def _run(self):
         self.is_running = False
         self.start()
 
-        #current_timestamp = time.time_ns() 
-        current_timestamp = ptp.get_ptp_timestamp(
-                            device_name = self.ptp_device,
-                            so_file=self.ptp_so_file)
+        if not self.ptp_device:
+            # Use Python system library
+            current_timestamp = time.time_ns()/1000000000
+
+        else: 
+            # Get it directly from PTP device
+            current_timestamp = ptp.get_ptp_timestamp(
+                                device_name = self.ptp_device,
+                                so_file=self.ptp_so_file)
 
         MESSAGE = f"{current_timestamp},{str(self.seq_n)}"
         self.sock.sendto(MESSAGE.encode(), (self.dst_ip, self.dst_port))
         self.seq_n = self.seq_n + 1
+
 
     def start(self):
         if not self.is_running:
             self._timer = Timer(self.interval, self._run)
             self._timer.start()
             self.is_running = True
+
 
     def stop(self):
         self._timer.cancel()
@@ -72,8 +89,7 @@ class UDP_sender(object):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ptp-so-file", type=str, default="./time_ops/ptp_time.so", 
-                        help='destination IP')
+
     parser.add_argument("--dest-ip", type=str, default="10.10.1.1", 
                         help='destination IP')
     parser.add_argument("--dest-port", type=int, default=5005, 
@@ -84,17 +100,23 @@ if __name__ == "__main__":
                         help="initial sequence number")
     parser.add_argument("--duration", type=int, default=60,
                         help="number of seconds to run")
+    parser.add_argument("--ptp-so-file", type=str, default="./time_ops/ptp_time.so", 
+                        help='path to ptp_time.so file.')
+    parser.add_argument("--sys-clock", action=argparse.BooleanOptionalAction,
+                        help='uses Python time module (mostly for debugging)')
+
     args = parser.parse_args()
     
-    ptp_so_file = args.ptp_so_file
     send_interval = args.frequency
     dest_ip = args.dest_ip
     dest_port = args.dest_port
     seq_n = args.seq_n
     duration = args.duration
+    ptp_so_file = args.ptp_so_file
+    sys_clock = args.sys_clock
 
-    owl_sender = UDP_sender(ptp_so_file, send_interval, 
-                            dest_ip, dest_port, seq_n)
+    owl_sender = UDP_sender(send_interval, dest_ip, dest_port, seq_n, 
+                            ptp_so_file, sys_clock)
     try:
         time.sleep(duration) # function should run during this time
     finally:
