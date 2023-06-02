a8467ae5c8c41b451619f0696e6245c691f7ba2e
mamihayashida <mami.hayashida@uky.edu>
2023-06-02 14:35:16 -0400

    new file dedicated to sending pcap data to InfluxDB.

---
+++ user_services/owl/owl/sock_ops/send_data.py
@@ -0,0 +1,119 @@
+from subprocess import Popen, PIPE
+from shlex import split
+import time
+import os
+import signal
+import re
+from decimal import *
+import argparse
+
+import influxdb_client 
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+'''
+Script for reading/tailing a pcap file, convert the relevant contents to ASCII, 
+and send data to InfluxDB. Intended for live-monitoring of OWL data.
+
+For more information on InfluxDB, go to https://www.influxdata.com/
+InfluxDB Python client: 
+    https://docs.influxdata.com/influxdb/cloud/api-guide/client-libraries/python/
+'''
+
+
+def parse_and_send(pcap_file, verbose=False, influxdb_token=None, 
+    influxdb_org=None, influxdb_url=None, influxdb_bucket=None):
+
+    # InfluxDB set up
+    write_client = influxdb_client.InfluxDBClient(url=influxdb_url, 
+                                token=influxdb_token, org=influxdb_org)
+    write_api = write_client.write_api(write_options=SYNCHRONOUS)
+
+
+    # Continuously read the end of a pcap file
+    cmd = f'tail -c +1 -f {pcap_file} | tcpdump -A -tt -n -l -r -'
+
+    if verbose:
+        print(f"Starting tcpdump session: ", cmd)
+
+    p1 = Popen(split(f"tail -c +1 -f {pcap_file}"), stdout=PIPE)
+    p2 = Popen(split("tcpdump -A -tt -n -l -r -"), stdin=p1.stdout, stdout=PIPE)
+
+
+    while True:
+        line = p2.stdout.readline()
+        if not line:
+            break
+    
+        newline = line.rstrip().decode()
+        IP_pos = newline.find("IP")
+    
+        # Find a line that looks like
+        # "1661899028.527932821 IP 10.10.2.1.40634 > 10.10.1.1.5005: UDP, length 23"
+        if IP_pos != -1:
+            packet_data = {}
+    
+            time_dst = newline[:(IP_pos-1)]
+            IPs = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', newline)
+    
+            packet_data["sender"] = IPs[0]
+            packet_data["receiver"] = IPs[1]
+            packet_data["received"] = time_dst
+    
+    
+        # Find a line that looks like ".........F1661899028.5274663,1877"
+        elif re.search(r'\d{10}.\d{,9},\d{1,4}$', newline):
+            parts = re.split(",", newline)
+            timestamp = re.findall('\d{10}\.\d{,9}', parts[0])
+            seq_n = parts[1]
+    
+            t_delta = (Decimal(time_dst) - Decimal(timestamp[0]))*1000000000
+    
+            packet_data["sent"] = timestamp[0]
+            packet_data["latency"] = t_delta
+            packet_data["seq"] = seq_n
+            packet_data["sent_ns"] = Decimal(timestamp[0])*1000000000
+            # Push the data to InfluxDB
+
+            point = (Point("owl")
+                    .tag("sender", packet_data["sender"])
+                    .tag("receiver", packet_data["receiver"])
+                    .field("received", float(packet_data["received"]))
+                    .field("latency", int(packet_data["latency"]))
+                    .field("seq_n", int(packet_data["seq"]))
+                    .time(int(packet_data["sent_ns"]), 
+                    write_precision=WritePrecision.NS)
+                    )        
+
+            write_api.write(bucket=influxdb_bucket, org=org, record=point)
+
+            if verbose:
+                print(packet_data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--pcapfile", type=str, default="/owl_output/owl.pcap",
+                        help="path/to/the/pcap/file")
+    parser.add_argument("--verbose", "-v", action='store_true', 
+                        help="verbose(print packet info)")
+    parser.add_argument("--token", type=str, help="influxdb token (str)")
+    parser.add_argument("--org", type=str, help="influxdb org name (str)")
+    parser.add_argument("--url", type=str, help="influxdb url (str)")
+    parser.add_argument("--bucket", type=str, help="influxdb bucket name (str)")
+    
+    args = parser.parse_args()
+    
+    pcap_file=args.pcapfile
+    verbose=args.verbose
+    token = args.token
+    org = args.org
+    url = args.url
+    bucket = args.bucket
+
+    parse_and_send(pcap_file, verbose=verbose, influxdb_token=token, influxdb_org=org, 
+                    influxdb_url=url, influxdb_bucket=bucket)
+
+
+
